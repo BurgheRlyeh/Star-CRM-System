c3e75490180c3a744879223df0c3909835206019
Alexandr <globaltouareg@gmail.com>
2023-04-12 13:38:52 +0500

    Updated bootstrap.yml

--- src/main/java/com/example/crm/config/ServiceConfig.java
+++ src/main/java/com/example/crm/config/ServiceConfig.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "example")
+@ConfigurationProperties("scholarservice")
 @Getter @Setter
 public class ServiceConfig {
     private String property;
